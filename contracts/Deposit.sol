8853a89aae972129d47f15010f33e7b3c7d03b84
Anthony Akentiev <anthony.akentiev@gmail.com>
2020-11-06 17:15:11 +0300

    Contracts continued

--- contracts/Deposit.sol
+++ contracts/Deposit.sol
@@ -5,13 +5,56 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Deposit {
 // fields:
 	address public addressCT;
+	ERC20 public token;
+	uint public depositAmount;
 
 // modifiers:
 	modifier onlyCT() {require(msg.sender == addressCT, "Access restricted"); _;}
 
 
 // methods:
-	constructor ( ERC20 _token ) public {
-		
+// 1 - CT
+	constructor ( address _addressCT, ERC20 _token, uint _depositAmount ) public {
+		addressCT = _addressCT;
+		token = _token;
+		depositAmount = _depositAmount;
+	}
+
+	function setToken( ERC20 _newToken ) external onlyCT() {
+		token = _newToken;
+	}
+
+	function setDepositAmount( uint _depositAmount ) external onlyCT() {
+		depositAmount = _depositAmount;
+	}
+
+	function releaseAllTokens() external onlyCT() {
+		// release all tokens from current contract to owners? 
+		// release all tokens from current contract to CT?
+
+	}
+
+	function isDepositedEnough( address _user ) external view returns(bool) {
+		// investor deposited enought tokens?
+		// depositAmount can be changed since then
+
+		return false;
+	} 
+
+// 2 - Investors:
+	function depositTokens() external {
+		// 1 - investor should allow us to withdraw X tokens
+
+		// 2 - if investor already deposited N tokens before 
+		// 2.1  - and 'depositAmount' increased -> require K=M-N more tokens
+
+		// 2.1  - and 'depositAmount' decreased -> DO NOT unlock excess tokens!
+
+		// 3 - if not enough -> fail
+	}
+
+	function releaseMyTokens() external {
+		// release all tokens from current contract to owner
+
 	}
 }
--- test/contracts/deposit.tests.js
+++ test/contracts/deposit.tests.js
@@ -1,20 +1,43 @@
 const BigNumber = web3.BigNumber;
 
-// const Deposit = artifacts.require('Deposit');
+const ERC20 = artifacts.require('ERC20');
+const Deposit = artifacts.require('Deposit');
 
 require('chai')
     .use(require('chai-as-promised'))
     .use(require('chai-bignumber')(BigNumber))
     .should();
 
-contract('Deposit', () => {
-    // const creator = accounts[0];
+contract('Deposit', (accounts) => {
+    const creator = accounts[0];
 
-    beforeEach(async () => {
+	beforeEach(async () => {
+		// this.erc20 = await ERC20.new("SomeToken", "STT");
+	});
 
-    });
 
     describe('(unit tests)', () => {
+        describe('contract constructor', () => {
+            it('should construct new contract instance', async function () {
+				this.erc20 = await ERC20.new("SomeToken", "STT");
+				this.deposit = await Deposit.new(creator, this.erc20.address, 15000);
 
+				assert.equal(await this.deposit.addressCT(), creator);
+				assert.equal(await this.deposit.token(), this.erc20.address);
+				assert.equal(await this.deposit.depositAmount(), 15000);
+            });
+        });
+
+        describe('setToken', () => {
+            it('should not update token address if called by someone', async function () {
+				this.erc20 = await ERC20.new("SomeToken", "STT");
+				this.deposit = await Deposit.new(creator, this.erc20.address, 15000);
+
+				// init new token
+				this.erc20_2 = await ERC20.new("SomeToken2", "STT2");
+				await this.deposit.setToken(this.erc20_2.address).should.be.rejectedWith('revert');
+				assert.equal(await this.deposit.token(), this.erc20.address);
+            });
+        });
     });
 });
